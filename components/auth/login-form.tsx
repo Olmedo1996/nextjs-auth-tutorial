16451baa7aded09266e285c4295c0c0837799b7d
diego <diegoolmedog96@gmail.com>
2024-09-02 20:27:47 -0400

    refactor: the login form is improved

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,38 @@
+"use client"
+import * as z from "zod";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import { CardWrapper } from "@/components/auth/card-wrapper";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form';
+import { LoginSchema } from "@/schemas";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
 
 export const LoginForm = () => {
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+    })
+
+    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+        console.log(values);
+        
+    }
     return (
         <CardWrapper
             headerLabel="Welcome back!"
@@ -8,7 +40,66 @@ export const LoginForm = () => {
             backButtonHref="/auth/register"
             showSocial
         >
-            <h1>LoginForm</h1>
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-6"
+                >
+                    <div className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="email"
+                                            /* autoComplete="email" */
+                                            {...field}
+                                            placeholder="joel@me.com"
+                                            /* className="w-full" */
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        We'll never share your email.
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="password"
+                                            {...field}
+                                            placeholder="******"
+                                            /* className="w-full" */
+                                        />
+                                    </FormControl>
+                                    {/* <FormDescription>
+                                        Your password must be at least 6 characters.
+                                    </FormDescription> */}
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <FormError message=""/>
+                    <FormSuccess message=""/>
+                    <Button
+                        type="submit"
+                        className="w-full"
+                    >
+                        Login
+                    </Button>
+                </form>
+            </Form>
         </CardWrapper>
     )
 }
